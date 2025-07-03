ebfac2b9ee441a1ab67f0618875ee5b865c770f5
Mykal-Steele <mykal.stele@gmail.com>
2025-07-03 23:53:34 +0700

    feat(frontend): setup basic routing to the temp page

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import { NavLink } from 'react-router';
 
 function Page() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  
+  return (
+    <div className="min-h-screen bg-black/90 flex flex-col items-center justify-center gap-8">
+        <h1 className="text-5xl font-bold text-white">CSFD 2025</h1>
+        <p className="text-lg text-white">Welcome</p>
+
+      <nav>
+        <NavLink 
+          to="/test" 
+          className="px-6 py-3 bg-white/80 text-black font-bold rounded hover:bg-white/60"
+        >
+          Go to Test Page
+        </NavLink>
+        
+      </nav>
+    </div>
+  );
 }
 
 export default Page;
