c0ab4a7ab098fa26bb68561245443a09a777605c
Pyae Sone Shin Thant <pssthant@gmail.com>
2025-07-04 12:06:13 +0700

    feat: add auth types and session data to enhance user management

---
+++ backend/src/types/auth.type.ts
@@ -0,0 +1,46 @@
+export interface User {
+  id: number;
+  displayName: string;
+  email: string;
+  nickname: string | null;
+  role: string;
+}
+
+export interface StudentInfo {
+  id: number;
+  displayName: string;
+  email: string;
+  nickname: string | null;
+  role: string;
+  lives: number;
+  mentees: Array<{
+    junior: {
+      id: string;
+      displayName: string;
+      nickname: string | null;
+    };
+  }>;
+  mentor?: {
+    senior: {
+      id: string;
+      displayName: string;
+      nickname: string | null;
+    };
+  } | null;
+  givenHints: Array<{
+    id: string;
+    order: number;
+    content: string;
+    revealDate: Date;
+  }>;
+  receivedHints: Array<{
+    id: string;
+    order: number;
+    content: string;
+    revealDate: Date;
+  }>;
+  house: string | null;
+  instagram: string | null;
+  discord: string | null;
+  line: string | null;
+}
