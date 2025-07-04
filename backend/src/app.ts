f628882d71815c833851d00757630703beb83dee
Pyae Sone Shin Thant <pssthant@gmail.com>
2025-07-04 12:53:32 +0700

    refactor: move prisma client initialization to its own module for better organization

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,11 +4,8 @@ import { router } from './routes';
 import compression from 'compression';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
-import { PrismaClient } from './generated/prisma';
-import { withAccelerate } from '@prisma/extension-accelerate';
 
 const app = express();
-export const prisma = new PrismaClient().$extends(withAccelerate());
 
 app.use(compression());
 app.use(cookieParser());
