402c09eb5ae316519b09e6b4466c50817c63a114
Pyae Sone Shin Thant <pssthant@gmail.com>
2025-07-05 18:43:50 +0700

    fix: update NotFoundError with custom message

--- backend/src/errors/not-found-error.ts
+++ backend/src/errors/not-found-error.ts
@@ -3,11 +3,11 @@ import { ErrorException } from '@/errors/error-exception';
 export class NotFoundError extends ErrorException {
   status = 404;
 
-  constructor() {
-    super('Route not found');
+  constructor(message: string = 'Not Found') {
+    super(message);
   }
 
   serializeErrors(): { message: string; field?: string | undefined }[] {
-    return [{ message: 'Not found' }];
+    return [{ message: this.message }];
   }
 }
