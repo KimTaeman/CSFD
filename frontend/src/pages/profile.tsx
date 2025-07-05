f78034f7210f031c5437b12946cfbf06be224576
Mykal-Steele <mykal.stele@gmail.com>
2025-07-05 23:56:15 +0700

    feat(frontend): Profile Page...

--- frontend/src/pages/profile.tsx
+++ frontend/src/pages/profile.tsx
@@ -1,15 +1,30 @@
 import { useState } from "react";
+import Picture from "@/components/picture";
+import isotarImage from "@/assets/img-placeholder.png";
 import Sidebar from "@/components/sidebar";
+import ProfileForm from "@/components/ProfileForm";
 
 function Page() {
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
   const closeSidebar = () => setIsSidebarOpen(false);
 
   return (
-    <div className="min-h-screen bg-cover bg-center bg-no-repeat md:bg-[url('/src/assets/bg-2.png')]">
-      <div className="ml-10">
-        <Sidebar isOpen={isSidebarOpen} onClose={closeSidebar} />
+    <div className="min-h-screen w-full bg-[url('frontend/src/assets/bg-1.svg')] bg-cover bg-center bg-no-repeat text-white flex">
+      {/* Sidebar */}
+      <div className="pl-10 pr-50">
+      {isSidebarOpen && <Sidebar isOpen={isSidebarOpen} onClose={closeSidebar} />}
       </div>
+
+      {/* Main Content */}
+      <main className="flex-1 p-17 grid grid-cols-12 gap-8 pl-25">
+        {/* Picture Upload Section */}
+        <div className="col-span-4 flex justify-center items-start pl-65">
+          <Picture src={isotarImage} alt="Profile" className="rounded-3xl w-220 h-140 " />
+        </div>
+
+        {/* Form Section */}
+        <ProfileForm />
+      </main>
     </div>
   );
 }
