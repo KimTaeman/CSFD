ac42b38c0eec54b0fb86cf918bd6d5618323f011
Nang Hayman Aye Mya <haymanayemya@gmail.com>
2025-07-16 22:38:13 +0700

    feat(frontend): implement main layout and update profile page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -7,6 +7,7 @@ import routes from '~react-pages';
 import LoadingLayout from '@/components/layout/loading.tsx';
 import ErrorLayout from '@/components/layout/error.tsx';
 import SidebarLayout from '@/components/layout/sidebar';
+import MainLayout from './components/layout/MainLayout';
 
 const container = document.getElementById('root');
 
@@ -21,9 +22,7 @@ export const App = () => {
   const PageContent = useRoutes(routes);
   return (
     <Suspense fallback={<LoadingLayout />}>
-      <SidebarLayout>
-        {PageContent}
-      </SidebarLayout>
+      <MainLayout>{PageContent}</MainLayout>
     </Suspense>
   );
 };
