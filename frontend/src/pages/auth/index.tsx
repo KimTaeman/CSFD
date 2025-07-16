5730df83219a7e7f18974bf1d8730e0df5b4934c
Pyae Sone Shin Thant <pssthant@gmail.com>
2025-07-16 12:28:13 +0700

    feat(auth): implement login page with Microsoft login button and background image.

--- frontend/src/pages/auth/index.tsx
+++ frontend/src/pages/auth/index.tsx
@@ -1,12 +1,23 @@
+import bg from '../../assets/bg-1.png';
+
 function HomePage() {
   const handleLogin = () => {
     window.location.href = `${import.meta.env.VITE_API_URL}/auth/login`;
   };
 
   return (
-    <div>
-      <h1>Testing</h1>
-      <button onClick={handleLogin}>Login with Microsoft</button>
+    <div
+      className="flex h-screen items-center justify-center bg-cover bg-center"
+      style={{ backgroundImage: `url(${bg})` }}
+    >
+      <div className="text-center">
+        <button
+          onClick={handleLogin}
+          className="cursor-pointer rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
+        >
+          Login with Microsoft
+        </button>
+      </div>
     </div>
   );
 }
