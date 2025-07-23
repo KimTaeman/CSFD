de27d2f0c9c12b0a4532b644d4310c9515df04d3
Theerawat Patthawee <smrmakeyt@gmail.com>
2025-07-24 01:01:27 +0700

    feat!: Removed support for Dockers

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,10 +4,32 @@ import { router } from './routes';
 import compression from 'compression';
 import cookieParser from 'cookie-parser';
 import session from 'express-session';
+import cors from 'cors';
 import config from './config/config';
 
 const app = express();
 app.set('trust proxy', 1);
+
+const allowedOrigins = [
+  'http://localhost:5173',
+  'http://localhost:3000',
+  'https://csfd.sit.kmutt.ac.th',
+];
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || typeof origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, origin);
+      }
+
+      return callback(new Error('Not allowed by CORS: ' + origin));
+    },
+    credentials: true,
+  }),
+);
 
 app.use(compression());
 app.use(cookieParser());
@@ -20,6 +42,7 @@ app.use(
     resave: false,
     saveUninitialized: false,
     cookie: {
+      maxAge: 15 * 60 * 1000,
       httpOnly: true,
       secure: config.nodeEnv === 'production',
       sameSite: 'lax',
