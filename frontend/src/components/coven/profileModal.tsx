5afb330ba435ed7fc435d5a55ddb46e94a2c9416
Theerawat Patthawee <smrmakeyt@gmail.com>
2025-07-24 21:06:55 +0700

    fix(frontend): Fixed house is null make the page breaks

--- frontend/src/components/coven/profileModal.tsx
+++ frontend/src/components/coven/profileModal.tsx
@@ -16,7 +16,7 @@ const ProfileModal: React.FC<ProfileModalProps> = ({ user, onClick }) => {
       )}
       <div className="w-auto transform rounded-2xl border border-white/30 bg-white text-[#1C1B1A] backdrop-blur-lg transition-all duration-500 ease-in-out hover:scale-[1.02] md:max-w-80">
         <div className="flex w-full flex-row items-center p-1">
-          <div className="mr-4 aspect-[5/7] max-h-[175px] w-full max-w-[120px] justify-center md:h-[100px] lg:w-[60px]">
+          <div className="mr-4 aspect-[5/7] max-h-[175px] max-w-[120px] justify-center md:h-[100px] lg:w-[60px]">
             <img
               className="h-full rounded-2xl object-cover"
               src={user.profilePic || `/assets/profile-${user.house}.png`}
