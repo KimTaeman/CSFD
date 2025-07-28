523addf8b75fc0ca790c22b89f444858650390e5
Nang Hayman Aye Mya <haymanayemya@gmail.com>
2025-07-29 00:37:21 +0700

    fix(hints): display "Revealed" instead of countdown when hint is shown.

--- frontend/src/pages/hints/index.tsx
+++ frontend/src/pages/hints/index.tsx
@@ -217,7 +217,11 @@ function Page() {
                     return (
                       <div key={hint.id}>
                         <p className="mb-2 text-center font-mono text-lg tracking-wider text-yellow-300/80">
-                          Reveal in {hintCountdown ? hintCountdown : 'Nuh Uh'}
+                          {hintCountdown
+                            ? `Reveal in ${hintCountdown}`
+                            : hint.content
+                              ? 'Revealed'
+                              : 'Nuh Uh'}
                         </p>
 
                         <HintCard
