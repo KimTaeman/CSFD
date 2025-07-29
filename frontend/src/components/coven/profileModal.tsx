bac0ce496a4f1652e3794ec13012d6d67848fa98
Pyae Sone Shin Thant <pssthant@gmail.com>
2025-07-29 14:20:29 +0700

    fix: remove aspect (for safari engine) in profile modal, popup, and upload components

--- frontend/src/components/coven/profileModal.tsx
+++ frontend/src/components/coven/profileModal.tsx
@@ -18,7 +18,7 @@ const ProfileModal: React.FC<ProfileModalProps> = ({ user, onClick, className })
       )}
       <div className="w-auto transform rounded-2xl border border-white/10 bg-gray-900/30 text-white shadow-lg backdrop-blur-lg transition-all duration-500 ease-in-out hover:scale-[1.02] md:max-w-80">
         <div className="flex w-full flex-row items-center p-1">
-          <div className="mr-4 aspect-[5/7] max-h-[175px] max-w-[120px] justify-center md:h-[100px] lg:w-[60px]">
+          <div className="mr-4 h-[175px] w-[120px] justify-center md:h-[100px] lg:w-[60px]">
             <img
               className="h-full rounded-2xl object-cover"
               src={user.profilePic || `/assets/profile-${user.house}.png`}
