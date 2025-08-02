c1bc9784e5d2097c34a808a9e22a956cb12d660e
Nang Hayman Aye Mya <haymanayemya@gmail.com>
2025-08-02 09:37:37 +0700

    feat(student.route.ts): remove unused route to get found pairs.

--- backend/src/routes/student.route.ts
+++ backend/src/routes/student.route.ts
@@ -13,7 +13,6 @@ router.get('/seniors', Controller.getAllSeniors);
 router.get('/admin/pairs', Controller.getMentorPairs);
 router.get('/:id', Controller.getStudentById);
 router.get('/:id/isCorrect', Controller.guessCorrect);
-router.get('/admin/found', Controller.getFoundPairs);
 
 // PUT
 router.put('/:id/guess', Controller.guessMentor);
