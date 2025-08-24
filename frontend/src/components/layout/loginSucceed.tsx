d0db20bbe918f6a170f5bfcbe0bc211c1066849e
Nang Hayman Aye Mya <haymanayemya@gmail.com>
2025-08-24 22:01:32 +0700

    feat(frontend): enhance login success notification and remove welcome page.

--- frontend/src/components/layout/loginSucceed.tsx
+++ frontend/src/components/layout/loginSucceed.tsx
@@ -1,12 +1,8 @@
 const LoginSuccess = () => {
   return (
-    <div className="w-auto max-w-sm transform rounded-3xl border border-white/30 bg-white/10 backdrop-blur-lg">
-      <div className="m-4 flex flex-row items-center space-x-4">
-        <img src="/assets/little-star.png" alt="little-star" className="h-5 w-5" />
-        <p className="font-inter text-effect text-[1.25rem] whitespace-nowrap text-white">
-          Login succeeded
-        </p>
-      </div>
+    <div className="flex flex-row items-center space-x-2 rounded-3xl border border-white/30 bg-white/10 px-3 py-3 backdrop-blur-lg md:space-x-4 md:px-6">
+      <img src="/assets/little-star.png" alt="little-star" className="h-5 w-5" />
+      <p className="font-inter text-md whitespace-nowrap text-white md:text-2xl">Login succeeded</p>
     </div>
   );
 };
